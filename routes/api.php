7897ab1a0cce5207f81d26708178e70ab9b946eb
keraneko <anbeplane@yahoo.co.jp>
2026-01-03 06:47:04 +0900

    フロントエンドからPOST通信

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\Api\ProductController;
-
+use App\Http\Controllers\OrderController;
 
 Route::get('/products', [ProductController::class, 'index']);
 
+Route::post('/orders', [OrderController::class, 'store']);
