21a4baf02476fbe9ea6165af4f77042bc5b29688
keraneko <anbeplane@yahoo.co.jp>
2026-01-11 07:01:03 +0900

    is_activeの追加とREADEMEの整形

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -4,11 +4,26 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use App\Http\Requests\StoreProductRequest;
+use App\Http\Requests\UpdateProductRequest;
 
 class ProductController extends Controller
 {
     public function index()
     {
-        return Product::select('id', 'name', 'price')->orderby('id')->get();
+        return Product::select('id', 'name', 'price','is_active')->orderby('id')->get();
+    }
+
+    public function store(StoreProductRequest $request)
+    {
+        $data = $request->validated();
+        $product = Product::create($data);
+        return ['product_id' => $product->id];
+    }
+
+    public function update(UpdateProductRequest $request, Product $product)
+    {
+        $product->update($request->validated());
+        return $product;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,11 @@ use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\StoreController;
 use App\Http\Controllers\OrderController;
 
+//product
 Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products',[ProductController::class, 'store']);
+Route::patch('/products/{product}',[ProductController::class, 'update']);
+
 Route::get('/stores', [StoreController::class, 'index']);
 
 Route::post('/orders', [OrderController::class, 'store']);
