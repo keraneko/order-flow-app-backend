0b9c5c8c1e5b647b6cdddb4d116c4891e213038f
keraneko <anbeplane@yahoo.co.jp>
2026-02-05 14:46:07 +0900

    Add store requests for validation

---
+++ app/Http/Requests/UpdateStoreRequest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+
+        $rules = [
+            'name' => ['required','string', 'min:1'],
+            'code' => ['required','string', 'digits:6',
+               Rule::unique('stores', 'code')->ignore($this->route('store')->id),],
+            'postal_code' => ['required', 'digits:7'],
+            'prefecture' => ['required','string'],
+            'city' => ['required','string'],
+            'address_line' => ['required','string'],
+            'is_active' => ['sometimes','boolean'],
+        ];
+        return $rules;
+
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $postal = $this->input('postal_code');
+
+        if(is_string($postal)){
+            $postal = mb_convert_kana($postal, 'n');
+            $postal = preg_replace('/\D+/','',$postal);
+
+            $this->merge([
+                'postal_code'=>$postal,
+            ]);
+        }
+
+        $code = $this->input('code');
+        if(is_string($code)){
+            $this->merge([
+                'code'=>mb_convert_kana($code, 'n')
+            ]);
+        }
+    }
+}
