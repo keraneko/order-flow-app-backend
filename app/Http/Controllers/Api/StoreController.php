a85fd47b17eee441313cc6db9424a2f98b4b4ba2
keraneko <anbeplane@yahoo.co.jp>
2026-02-05 14:46:42 +0900

    Implement store API endpoints

--- app/Http/Controllers/Api/StoreController.php
+++ app/Http/Controllers/Api/StoreController.php
@@ -3,12 +3,51 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreStoreRequest;
+use App\Http\Requests\UpdateStoreRequest;
 use App\Models\Store;
 
 class StoreController extends Controller
 {
     public function index()
     {
-        return Store::select('id', 'name')->orderby('id')->get();
+        return Store::select('id', 'name','code','postal_code','prefecture','city','address_line','is_active',)->orderby('id')->get();
+    }
+
+    public function store(StoreStoreRequest $request)
+    {
+        $store = Store::create($request->validated());
+        return Store::select(
+            'id',
+            'name',
+            'code',
+            'postal_code',
+            'prefecture',
+            'city',
+            'address_line', 
+            'is_active',
+            )->where('id',$store->id)->first();
+    }    
+
+    public function show(Store $store )
+    {
+        return $store;
+    }
+
+    public function update(UpdateStoreRequest $request, Store $store)
+    {
+        $data = $request->validated();
+        $store->update($data);
+
+        return Store::select(
+            'id',
+            'name',
+            'code',
+            'postal_code',
+            'prefecture',
+            'city',
+            'address_line', 
+            'is_active',
+            )->where('id',$store->id)->first();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,17 @@ use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\StoreController;
 use App\Http\Controllers\OrderController;
 
-//product
+//products
 Route::get('/products', [ProductController::class, 'index']);
 Route::post('/products',[ProductController::class, 'store']);
 Route::get('/products/{product}', [ProductController::class, 'show']);
 Route::patch('/products/{product}',[ProductController::class, 'update']);
 Route::delete('/products/{product}',[ProductController::class, 'destroy']);
 
+//Stores
 Route::get('/stores', [StoreController::class, 'index']);
+Route::post('/stores', [StoreController::class, 'store']);
+Route::get('/stores/{store}', [StoreController::class, 'show']);
+Route::patch('/stores/{store}',[StoreController::class, 'update']);
 
 Route::post('/orders', [OrderController::class, 'store']);
