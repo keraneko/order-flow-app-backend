f44e1d03dd177a56aa0eaa2f345dd569837da10f
keraneko <anbeplane@yahoo.co.jp>
2026-04-26 15:11:16 +0900

    feat: restrict order delivery type updates by role and store

--- app/Http/Controllers/OrderDeliveryTypeController.php
+++ app/Http/Controllers/OrderDeliveryTypeController.php
@@ -10,14 +10,23 @@ class OrderDeliveryTypeController extends Controller
 {
     public function update(UpdateOrderDeliveryTypeRequest $request, Order $order)
     {
+        $user = $request->user();
+        $canUpdate = ($user->role === 'admin') ||
+            (
+                $user->role === 'store_user' &&
+                $user->store_id === $order->order_store_id
+            );
+        if(!$canUpdate){
+            abort(403);
+        }
+
         $deliveryType = $request->validated();
-        
 
         if($deliveryType['delivery_type'] === 'pickup'){
             $date = $order->delivery_date;
             $from = $order->delivery_from;
             $dt = Carbon::createFromFormat('Y-m-d H:i:s', $date.' '.$from);
-            $to = $dt->copy()->addMinutes(30)->format('H:i');
+            $to = $dt->copy()->addMinutes(30)->format('H:i:s');
             
             $order->update([
                 'delivery_type' => $deliveryType['delivery_type'],
