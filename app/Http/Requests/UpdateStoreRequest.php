687642679c06d4dcc605db3bd0dfb9837ac8bed0
keraneko <anbeplane@yahoo.co.jp>
2026-05-04 14:12:08 +0900

    fix: add store validation rules

--- app/Http/Requests/UpdateStoreRequest.php
+++ app/Http/Requests/UpdateStoreRequest.php
@@ -24,7 +24,7 @@ class UpdateStoreRequest extends FormRequest
     {
 
         $rules = [
-            'name' => ['required','string', 'min:1'],
+            'name' => ['required','string'],
             'code' => ['required','string', 'digits:6',
                Rule::unique('stores', 'code')->ignore($this->route('store')->id),],
             'postal_code' => ['required', 'digits:7'],
@@ -66,10 +66,12 @@ class UpdateStoreRequest extends FormRequest
             'code.digits' => '店コードは６桁です',
             'code.unique' => '他店と同じコードは使用できません',
             'postal_code.required' => '郵便番号は必須です',
-            'postal_code.digits' => '郵便番号はハイフンなしの７桁で入力してください',
+            'postal_code.digits' => '郵便番号は7桁の数字で入力してください',
             'prefecture.required' => '都道府県を入力してください',
             'city.required' => '市町村を入力してください',
             'address_line.required' => '番地以下を入力してください',
+            'is_active.required' => '公開/非公開は必須です',
+            'is_active.boolean' => '公開/非公開の値が正しくありません',
         ];
     }
 }
